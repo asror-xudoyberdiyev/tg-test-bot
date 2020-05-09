03e35160f00013eff5732de3ec0f92071c8a047b
asror-xudoyberdiyev <64538466+asror-xudoyberdiyev@users.noreply.github.com>
2020-05-09 16:53:14 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const token = '1181526310:AAFNIDtaqTNE8Ulng-DanWyAf96m0pcqSgY';
 const bot = new telegramBot(token,{polling: true})
 
 bot.on('message',msg=>{
-bot.sendMessage(msg.chat.id,"Hello! I am working for you :)")
+bot.sendMessage(msg.chat.id,"working...")
 })
